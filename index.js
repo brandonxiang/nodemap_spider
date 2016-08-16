2b85185fd74d77b6552cac165651d6682b8e60f4
brandonxiang <1542453460@qq.com>
2016-08-16 13:05:26 +0800

    download function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var request = require('request');
 var util = require('util');
+var path = require('path');
 
 
 URL = {
@@ -15,37 +16,68 @@ URL = {
 }
 
 
+var processTilenum = function (left, right, top, bottom, zoom, output, maptype) {
+    output = output || 'mosaic';
+    maptype = maptype || "default";
+    download(left, right, top, bottom, zoom, output, maptype);
+}
 
-
-var download = function(left, right, top, bottom, z, filename, maptype){
-    maptype = maptype||"default";
-     for (var x = left; x < right+1; x++) {
-         for (var y = top; y < bottom+1; y++) {
-             var path =  './tiles/{filename}/{z}/{x}/{y}.png'.format({x:x,y:y,z:z,filename:filename});
-             if(fs.accessSync(path)){
-                 _download(x,y,z,filename,maptype);
-             }
-         }
-     }
+var download = function (left, right, top, bottom, z, filename, maptype) {
+    maptype = maptype || "default";
+    for (var x = left; x < right + 1; x++) {
+        for (var y = top; y < bottom + 1; y++) {
+            var path = 'tiles/{filename}/{z}/{x}/{y}.png'.format({ x: x, y: y, z: z, filename: filename });
+                _download(x, y, z, path, maptype);
+        }
+    }
 }
 
 var _download = function (x, y, z, filename, maptype) {
-    var filepath = "./tiles/test.png"
-    var url= URL[maptype].format({x:x,y:y,z:z});
-    request.head(url, function (err, res, body) {
-        if (err) {
-            console.log('err:' + err);
-        }
-        request(url).pipe(fs.createWriteStream(filepath));
-    });
+    // var filepath = "./tiles/test.png"
+    var url = URL[maptype].format({ x: x, y: y, z: z });
+    var pathname = path.dirname(filename);
+    mkdirsSync(pathname);
+    if (!fs.existsSync(filename)) {
+        request.head(url, function (err, res, body) {
+            if (err) {
+                console.log('get:' + err);
+                return;
+            }
+            request(url).pipe(fs.createWriteStream(filename));
+            console.log('downloaded: '+ filename);
+        });
+    }
 };
 
-String.prototype.format = function(json){
+String.prototype.format = function (json) {
     var temp = this;
-    for(var key in  json){
-        temp = temp.replace("{"+key+"}",json[key]);
+    for (var key in json) {
+        temp = temp.replace("{" + key + "}", json[key]);
     }
     return temp;
 }
 
-_download();
+function mkdirsSync(dirpath, mode) { 
+    if (!fs.existsSync(dirpath)) {
+        var pathtmp;
+        dirpath.split("/").forEach(function(dirname) {
+            if (pathtmp) {
+                pathtmp = path.join(pathtmp, dirname);
+            }
+            else {
+                pathtmp = dirname;
+            }
+            if (!fs.existsSync(pathtmp)) {
+                if (!fs.mkdirSync(pathtmp, mode)) {
+                    return false;
+                }
+            }
+        });
+    }
+    return true; 
+}
+
+// _download();
+ processTilenum(803,857,984,1061,8,'WORKNET')
+
+
